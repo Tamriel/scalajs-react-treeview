b856d28b99257cf0c15e57ec8b1106a3c3ff6edd
Jan <j.korte@me.com>
2017-07-19 15:52:34 +0200

    adding children

--- src/main/scala/app/TreeView.scala
+++ src/main/scala/app/TreeView.scala
@@ -3,34 +3,45 @@ package app
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
-case class Node(text: Int, children: Vector[Node])
+case class Node(text: String, children: Vector[Node])
 
 object TreeView {
-  val childNode = Node(2, Vector())
-  val parentNode = Node(1, Vector(childNode))
+  val childNode = Node("1.1", Vector())
+  val parentNode = Node("1", Vector(childNode))
 
-  val nodeComponent = ScalaComponent
-    .builder[Node]("Node")
+  val rootNode = ScalaComponent
+    .builder[Unit]("Node")
+    .initialState(parentNode)
     .renderBackend[NodeBackend]
     .build
 
-  class NodeBackend($ : BackendScope[Node, Unit]) {
+  class NodeBackend($ : BackendScope[Unit, Node]) {
+
+    def addChild =
+      $.modState(
+        _.copy(children = $.state.runNow().children :+ Node("1.2", Vector())))
 
     def render(node: Node): VdomElement = {
       val children =
         if (node.children.nonEmpty)
-          node.children.toVdomArray(child =>
-            nodeComponent.withKey(child.text)(child))
+          node.children.toVdomArray(child => {
+            val childNode = ScalaComponent
+              .builder[Unit]("Node")
+              .initialState(child)
+              .renderBackend[NodeBackend]
+              .build
+            childNode.withKey(child.text)()
+          })
         else EmptyVdom
 
       <.div(
         node.text,
         <.input(),
-        <.button("+1"),
+        <.button("Add child", ^.onClick --> addChild),
         children
       )
     }
   }
 
-  def apply() = nodeComponent(parentNode)
+  def apply() = rootNode()
 }
